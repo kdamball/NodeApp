bd7fa6210fbfb731423404f32bb9118ec9b4bb71
kdamball <kadodamball@hotmail.com>
2014-02-28 16:08:40 +0300

    Tried fixing errors with formidable but drawing a blank. fuck it

--- requestHandler.js
+++ requestHandler.js
@@ -1,7 +1,6 @@
 var querystring = require("querystring"),
     fs = require("fs"),
-    formidable = require("formidable"),
-    body = "";
+    formidable = require("formidable");
 
 function start(response){
 	console.log("Request handler 'start' was called");
@@ -25,22 +24,22 @@ function start(response){
 function upload(response, request){
 	console.log("Request handler 'upload' was called");
   
-  var form = formidable.IncomingForm();
+  var form = new formidable.IncomingForm();
   console.log("about to parse");
   form.parse(request, function(error, fields, files){
     console.log("parsin done");
     /*
       Possible Windows error: tried to rename an existing file
     */
-    fs.rename(files.upload.path, "/tmp/test.png", function(error){
-      if (error){
-        //fs.unlink("/tmp/test.png");
-        fs.rename(files.upload.path, "/tmp/test.png");
+    fs.rename(files.upload.path, "tmp/test.png", function(err){
+      if (err){
+        fs.unlink("tmp/test.png");
+        fs.rename(files.upload.path, "tmp/test.png");
       }
     });
     response.writeHead(200, {"Content Type": "text/html"});
     response.write("Receiving image: <br>")
-    response.write("<img src='/show/' />");
+    response.write("<img src='/show' />");
     response.end();
   });
 };
@@ -48,7 +47,7 @@ function upload(response, request){
 function show(response){
   console.log("Request handler 'show' was called.");
   response.writeHead(200, {"Content Type": "image/png"});
-  fs.createReadStream("tmp/test.png").pipe(response);
+  fs.createReadStream("./tmp/test.png").pipe(response);
 };
 
 exports.start = start;
