4a17f7304a19840674427ca0d4f0fdcdec3dc6d0
Kado Damball <kadodamball@hotmail.com>
2014-06-20 16:18:19 +0300

    Chat Server

--- node_notes/test.js
+++ node_notes/test.js
@@ -167,6 +167,31 @@ var fs = require('fs');
   }).listen(4001); 
 */
 
+// Chat App 
+  var net = require('net');
+  var server = net.createServer();
+  var sockets = [];
+  server.on('connection', function(socket) {
+    console.log('got a new connection');
+    sockets.push(socket);
+    socket.write("Welcome!");
+    socket.on('data', function(data) {
+      // console.log('got data:', data.toString());
+      sockets.forEach(function(otherSocket) {
+        if (otherSocket !== socket) {
+          otherSocket.write(data.toString());
+        }
+      });
+    });
+
+  });
+  server.on('error', function(err) {
+    console.log('Server error:', err.message);
+  });
+  server.on('close', function() {
+    console.log('Server closed');
+  });
+  server.listen(4001);
 
 
 
