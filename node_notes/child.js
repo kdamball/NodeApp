f3ece42bf88d2d9b289c444f7d4ff08e2a3df155
Kado Damball <kadodamball@hotmail.com>
2014-07-07 16:12:51 +0300

    HTTP Requests, done

--- node_notes/child.js
+++ node_notes/child.js
@@ -1,3 +1,24 @@
-var number = process.env.number;
-console.log(typeof number);
-console.log(typeof (Number(number)));
+/* //Child Process example
+  var number = process.env.number;
+  console.log(typeof number);
+  console.log(typeof (Number(number)));
+ */
+ 
+ 
+/* //Request Example
+  var request = require('request');
+  var inspect = require('util').inspect;
+  request(
+    'http://localhost:4001/redirect',
+    function(err, res, body) {
+      if (err) { throw err; }
+      console.log(inspect({
+        err: err,
+        res: {
+          statusCode: res.statusCode
+        },
+        body: JSON.parse(body)
+      }))
+    }
+  );
+ */
