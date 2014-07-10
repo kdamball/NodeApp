b6ee06124b5b88f6a4a33144d7ddfe1949465061
Kado Damball <kadodamball@hotmail.com>
2014-07-11 00:08:29 +0300

    Removed silly files

--- misc_node_scripts/testing.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs"),
-    request = require("request");
-    
-file = fs.createWriteStream("resume.html");
-
-request("http://google.com/search?q="+"all+my+troubles+my+shoulders+turn+to+black+heart+lyrics", function(err, res, body){
-}).pipe(file)
