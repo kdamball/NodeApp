51424e7248d41443699b4bbf6b17c5f038735499
Kado Damball <kadodamball@hotmail.com>
2014-07-29 23:57:14 +0300

    Cleaned up the zantel file

--- misc_node_scripts/zantel.js
+++ misc_node_scripts/zantel.js
@@ -1,11 +1,12 @@
-var request = require("request");
+var request = require("request"),
+    number = "0775061241",
+    monthly = "Month 12GB";
 
 var recharge = function(){ 
   request.post("http://www.zantel.com/ezyrecharge/func.php", function(err, res, body){
-    // console.log(res.statusCode);
     console.log(body);
   }).form({
-    "msisdn": "0775061241",
+    "msisdn": number,
     "voucherID": "1096602636699",
     "custName": "",
     "custEmail": "",
@@ -15,22 +16,20 @@ var recharge = function(){
 
 var enrollBundle = function(){
   request.post("http://www.zantel.com/ezyrecharge/func.php", function(err, res, body){
-    // console.log(res.statusCode);
     console.log(body);
   }).form({
-    "msisdn": "0775061241",
+    "msisdn": number,
     "gsm": "775061241",
-    "bundle": "Month 12GB",
+    "bundle": monthly,
     "func": "EnrollBundleCDMA"
   });
 }
 
 var checkBalance = function(){
   request.post("http://www.zantel.com/ezyrecharge/func.php", function(err, res, body){
-    // console.log(res.statusCode);
     console.log(body);
   }).form({
-    "msisdn": "0775061241",
+    "msisdn": number,
     "msType": "2",
     "func": "CheckBalance"
   });
