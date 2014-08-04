65cab0dfae75c2c97b59aefd3377614e9bc9e330
Kado Damball <kadodamball@hotmail.com>
2014-08-04 03:37:03 +0300

    Async queuing

--- node_notes/test.js
+++ node_notes/test.js
@@ -426,6 +426,37 @@ async.waterfall([
 ], done);
 */
 
+/* //Async Queue
+  var async = require('async'),
+      request = require('request');
+  function done(err, results) {
+    if (err) {
+      throw err;
+    }
+    console.log('results: %j', results);
+  }
+  var maximumConcurrency = 5;
+  function worker(task, callback) {
+    request.post({
+      uri: 'http://localhost:8080',
+      body: JSON.stringify(task)},
+      function(err, res, body) {
+        callback(err, body && JSON.parse(body));
+      }
+    );
+  }
+  var queue = async.queue(worker, maximumConcurreny);
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach(function(i) {
+    queue.push(i, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log(i + '^2 = %d', result);
+    });
+  });
+
+ */
+
 
 
 
