d0152d0b1bab64829ac4bb81e17716dcde0f579b
Kado Damball <kadodamball@hotmail.com>
2014-08-09 01:14:43 +0300

    Connect serving static files

--- node_notes/test.js
+++ node_notes/test.js
@@ -467,10 +467,18 @@ async.waterfall([
     replyText('Hello World!')
   );
   app.listen(8080);
-
  */
-
-
+ 
+ 
+/* //Static file server
+  var connect = require("connect");
+  var app = connect();
+  app.use(connect.static(__dirname+"/public"));
+  app.use(function(req, res){
+    res.end("Hello, World!");
+  });
+  app.listen(8080)
+ */
 
 
 
